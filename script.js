8c3d51b7625c21ddefb970a61dccb63bd9302871
Mariia Ushakova <m.ushakova22@gmail.com>
2024-02-01 13:58:48 +0100

    feat: add analyzer

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-const audio = document.getElementById("audio");
 const file = document.getElementById("file");
+const audio = document.createElement("audio");
+const visualizerContainer = document.querySelector(".grid");
+const numberOfColumns = 6;
 
-file.addEventListener("change", handleFiles, false);
+function printGrid() {
+  for (let i = 0; i < numberOfColumns; i++) {
+    const bar = document.createElement("div");
+    bar.setAttribute("id", "bar" + i);
+    bar.setAttribute("class", "grid__bar");
+    visualizerContainer.appendChild(bar);
+  }
+}
 
 function handleFiles() {
   const files = file.files;
@@ -9,6 +18,40 @@ function handleFiles() {
     console.log("No file chosen");
   } else {
     const file = files[0];
+    audio.id = "audio-player";
+    audio.controls = "controls";
     audio.src = URL.createObjectURL(file);
+    document.getElementById("container").appendChild(audio);
   }
 }
+
+function createAnalyzer() {
+  const ctx = new AudioContext();
+  const audioSource = ctx.createMediaElementSource(audio);
+  const analayzer = ctx.createAnalyser();
+  audioSource.connect(analayzer);
+  audioSource.connect(ctx.destination);
+  const frequencyData = new Uint8Array(analayzer.frequencyBinCount);
+  analayzer.getByteFrequencyData(frequencyData);
+  renderFrame(frequencyData);
+}
+
+function renderFrame(frequencyData) {
+  for (let i = 0; i < numberOfColumns; i++) {
+    const index = (i + 10) * 2;
+    const fd = frequencyData[index];
+    const bar = document.querySelector("#bar" + i);
+    if (!bar) {
+      continue;
+    }
+
+    const barHeight = Math.max(4, fd || 0);
+    bar.style.height = barHeight + "px";
+  }
+
+  window.requestAnimationFrame(renderFrame);
+}
+
+window.addEventListener("load", printGrid);
+file.addEventListener("change", handleFiles);
+audio.addEventListener("play", createAnalyzer);
