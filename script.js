3cecd4eeb9d58263fc60cb76b5399c9be4f8b1db
Mariia Ushakova <m.ushakova22@gmail.com>
2024-02-02 14:58:48 +0100

    feat: add grid and change colors according music

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 const file = document.getElementById("file");
 const audio = document.createElement("audio");
 const visualizerContainer = document.querySelector(".grid");
-const numberOfColumns = 6;
+const ctx = new AudioContext();
+const analyser = ctx.createAnalyser();
+analyser.fftSize = 32;
+const bufferLength = analyser.frequencyBinCount;
+const frequencyData = new Uint8Array(bufferLength);
+const numberOfColumns = 13;
 
 function printGrid() {
   for (let i = 0; i < numberOfColumns; i++) {
     const bar = document.createElement("div");
-    bar.setAttribute("id", "bar" + i);
+    bar.setAttribute("id", "column" + i);
     bar.setAttribute("class", "grid__bar");
     visualizerContainer.appendChild(bar);
+    const column = document.getElementById("column" + i);
+    for (let j = numberOfColumns - 1; j >= 0; j--) {
+      const row = document.createElement("div");
+      row.setAttribute("id", "column" + i + "row" + j);
+      row.setAttribute("class", "grid__bar");
+      column.appendChild(row);
+    }
   }
 }
 
@@ -18,40 +30,57 @@ function handleFiles() {
     console.log("No file chosen");
   } else {
     const file = files[0];
-    audio.id = "audio-player";
-    audio.controls = "controls";
-    audio.src = URL.createObjectURL(file);
-    document.getElementById("container").appendChild(audio);
+    if (file.type === "audio/mpeg") {
+      audio.controls = "controls";
+      audio.src = URL.createObjectURL(file);
+      document.getElementById("container").appendChild(audio);
+      createAnalyzer();
+    } else {
+      console.log(
+        "Selected file format is not supported. Choose another audio file"
+      );
+    }
   }
 }
 
 function createAnalyzer() {
-  const ctx = new AudioContext();
   const audioSource = ctx.createMediaElementSource(audio);
-  const analayzer = ctx.createAnalyser();
-  audioSource.connect(analayzer);
-  audioSource.connect(ctx.destination);
-  const frequencyData = new Uint8Array(analayzer.frequencyBinCount);
-  analayzer.getByteFrequencyData(frequencyData);
-  renderFrame(frequencyData);
+  ctx.resume();
+  audioSource.connect(analyser);
+  analyser.connect(ctx.destination);
 }
 
-function renderFrame(frequencyData) {
+function renderFrame() {
+  analyser.getByteFrequencyData(frequencyData);
+  console.log(frequencyData);
   for (let i = 0; i < numberOfColumns; i++) {
-    const index = (i + 10) * 2;
-    const fd = frequencyData[index];
-    const bar = document.querySelector("#bar" + i);
-    if (!bar) {
-      continue;
+    for (let j = 0; j < numberOfColumns; j++) {
+      const row = document.getElementById("column" + i + "row" + j);
+      if (j < Math.ceil(frequencyData[i] % numberOfColumns)) {
+        row.classList.add("non__empty");
+      } else if (row.classList.contains("non__empty")) {
+        row.classList.remove("non__empty");
+      }
     }
-
-    const barHeight = Math.max(4, fd || 0);
-    bar.style.height = barHeight + "px";
   }
 
   window.requestAnimationFrame(renderFrame);
 }
 
+function pauseAudio() {
+  // here should be method for pause connection. I haven't found yet
+  //ctx.close();
+  window.cancelAnimationFrame(renderFrame);
+}
+
+function deleteAnalyzer() {
+  audioSource.disconnect(analyser);
+  analyser.disconnect(ctx.destination);
+  ctx.close();
+}
+
 window.addEventListener("load", printGrid);
 file.addEventListener("change", handleFiles);
-audio.addEventListener("play", createAnalyzer);
+audio.addEventListener("play", renderFrame);
+audio.addEventListener("pause", pauseAudio);
+audio.addEventListener("ended", deleteAnalyzer);
